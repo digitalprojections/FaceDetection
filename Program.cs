bb41a03d89f3c656620e284f6733a9aa7c65077e
digitalprojects <fuzalov@gmail.com>
2019-07-24 16:40:11 +0900

    working after issues
    working

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using Microsoft.VisualBasic.ApplicationServices;
+using System.Reflection;
 
 namespace FaceDetection
 {
@@ -23,6 +24,7 @@ namespace FaceDetection
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
             //Application.Run(new mainForm(vs));
             IReadOnlyCollection<string> vs1 = (IReadOnlyCollection<string>) vs;
 
@@ -34,10 +36,28 @@ namespace FaceDetection
             }
             else
             {
-                SingleInstanceApplication.Run(new MainForm(vs1), NewInstanceHandler);
-                MainForm.handleParameters(vs1);
+                try
+                {
+                    SingleInstanceApplication.Run(new MainForm(vs1), NewInstanceHandler);
+                    MainForm.handleParameters(vs1);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.ToString());
+                }
             }
         }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            using (var str = Assembly.GetExecutingAssembly().GetManifestResourceStream("FaceDetection.OpenH264Lib.dll"))
+            {
+                byte[] assemblyData = new byte[str.Length];
+                str.Read(assemblyData, 0, assemblyData.Length);
+                return Assembly.Load(assemblyData);
+            }
+        }
+
         public static void NewInstanceHandler(object sender, StartupNextInstanceEventArgs e)
         {
             var uvc_param = e.CommandLine;
