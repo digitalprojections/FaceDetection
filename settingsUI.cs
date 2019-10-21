7dd9fc3655864ffca8d963b066d650562b6b490c
digitalprojects <fuzalov@gmail.com>
2019-10-21 09:48:55 +0900

    updated settingsUI only

--- settingsUI.cs
+++ settingsUI.cs
@@ -28,19 +28,24 @@ namespace FaceDetection
         public int Camera_index { get { return camera_index = Properties.Settings.Default.current_camera_index; } private set { Properties.Settings.Default.current_camera_index = value; Properties.Settings.Default.Save(); } }
         public int Camera_number { get { return camera_number = Properties.Settings.Default.current_camera_index+1;  } }
 
-        
+        private int InitPanelWidth;
+        private int InitPanelHeight;
 
         public settingsUI()
         {
+            Directory.CreateDirectory(Properties.Settings.Default.video_file_location);
+            Directory.CreateDirectory(Properties.Settings.Default.video_file_location + "/Camera");
+
             InitializeComponent();
             //Setup window
-            this.Width = 1080;
+            this.Width = 1024;
             this.Height = 760;
             this.ControlBox = false;
+            
+            InitPanelWidth = this.flowLayoutPanel1.Width;
+            InitPanelHeight = this.flowLayoutPanel1.Height;
 
             //get reference to the mainform
-            
-            
             Console.WriteLine(MainForm.GetCamera());
 
         }       
@@ -79,9 +84,11 @@ namespace FaceDetection
         {
             try
             {
-                Directory.CreateDirectory(Properties.Settings.Default.video_file_location);
-                Directory.CreateDirectory(Properties.Settings.Default.video_file_location + "/Camera");
-                Process.Start(Properties.Settings.Default.video_file_location);
+                this.TopMost = false;
+                //Directory.CreateDirectory(Properties.Settings.Default.video_file_location);
+                //Directory.CreateDirectory(Properties.Settings.Default.video_file_location + "/Camera");
+                //Process.Start(Properties.Settings.Default.video_file_location);
+                changeStoreLocation(sender, e);
             }
             catch (IOException ioe)
             {
@@ -155,11 +162,6 @@ namespace FaceDetection
             SetCameraPropertiesFromMemory();
         }
 
-        private void Label10_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void SettingsUI_Load(object sender, EventArgs e)
         {
             if (cm_camera_number.Items.Count>0)
@@ -198,11 +200,146 @@ namespace FaceDetection
                         ArrangeCameraNames(capDevices.Length);
                     }
                 }
+                PicBoxInitFunction();
         }
+
+        private void PicBoxInitFunction()
+        {
+            if (cb_all_cameras.Checked == true)
+            {
+                pictureBox_allcam.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_allcam.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_delete_old.Checked == true)
+            {
+                pictureBox_DeleteOldData.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_DeleteOldData.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_always_on_top.Checked == true)
+            {
+                pictureBox_alwaysOnTop.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_alwaysOnTop.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_window_pane.Checked == true)
+            {
+                pictureBox_showWindowPane.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_showWindowPane.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_dateandtime.Checked == true)
+            {
+                pictureBox_showCurrentDate.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_showCurrentDate.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_show_camera_number.Checked == true)
+            {
+                pictureBox_showCameraNo.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_showCameraNo.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_show_rec_icon.Checked == true)
+            {
+                pictureBox_showRecordingIcon.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_showRecordingIcon.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_face_recognition.Checked == true)
+            {
+                pictureBox_faceRecognition.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_faceRecognition.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_backlight_on_recognition.Checked == true)
+            {
+                pictureBox_BacklightOnRecognition.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_BacklightOnRecognition.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_operator_capture.Checked == true)
+            {
+                pictureBox_operatorCapture.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_operatorCapture.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_recording_during_facerec.Checked == true)
+            {
+                pictureBox_recordingDuringFaceRecognition.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_recordingDuringFaceRecognition.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_record_upon_start.Checked == true)
+            {
+                pictureBox_recordUponStart.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_recordUponStart.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_backlight_off_idling.Checked == true)
+            {
+                pictureBox_backlightOffWhenIdle.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_backlightOffWhenIdle.Image = Properties.Resources.checkered;
+            }
+
+            if (cb_event_recorder.Checked == true)
+            {
+                pictureBox_EventRecorder.Image = Properties.Resources.check;
+            }
+            else
+            {
+                pictureBox_EventRecorder.Image = Properties.Resources.checkered;
+            }
+        }
+
 
         private void Cm_language_SelectedIndexChanged(object sender, EventArgs e)
         {            
             ChangeLanguage();
+
+            //this.flowLayoutPanel1.Width = InitPanelWidth;
+            //this.flowLayoutPanel1.Height = InitPanelHeight;
+            //this.flowLayoutPanel1.Refresh();
         }
 
         private void ChangeLanguage()
@@ -221,8 +358,9 @@ namespace FaceDetection
             string lan = Properties.Settings.Default.culture;
             ComponentResourceManager resources = new ComponentResourceManager(typeof(settingsUI));
             var cult = new CultureInfo(lan);
-            
+
             foreach (Control c in this.Controls)
+            //foreach (Control c in this.flowLayoutPanel1.Controls)
             {                
                 resources.ApplyResources(c, c.Name, cult);
                 Debug.WriteLine(c.GetType().ToString());
@@ -231,6 +369,7 @@ namespace FaceDetection
                     checkOnKids(cult, c, resources);
                 }
             }
+
         }
         private void checkOnKids(CultureInfo cult, Control control, ComponentResourceManager crm)
         {
@@ -263,9 +402,191 @@ namespace FaceDetection
             Properties.Settings.Default.Save();
         }
 
+        private void cb_all_cameras_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_all_cameras, pictureBox_allcam);
+        }
+
         private void PictureBox_allcam_Click(object sender, EventArgs e)
         {
-            pictureBox_allcam.Image = Properties.Resources.check;
+            CB_Chenge(cb_all_cameras);
+            cb_all_cameras_CheckedChanged(sender, e);
         }
+
+        private void cb_delete_old_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_delete_old, pictureBox_DeleteOldData);
+        }
+
+        private void pictureBox_DeleteOldData_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_delete_old);
+            cb_delete_old_CheckedChanged(sender, e);
+        }
+
+        private void cb_always_on_top_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_always_on_top, pictureBox_alwaysOnTop);
+        }
+
+        private void pictureBox_alwaysOnTop_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_always_on_top);
+            cb_always_on_top_CheckedChanged(sender, e);
+        }
+
+        private void cb_window_pane_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_window_pane, pictureBox_showWindowPane);
+        }
+
+        private void pictureBox_showWindowPane_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_window_pane);
+            cb_window_pane_CheckedChanged(sender, e);
+        }
+
+        private void cb_dateandtime_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_dateandtime, pictureBox_showCurrentDate);
+        }
+
+        private void pictureBox_showCurrentDate_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_dateandtime);
+            cb_dateandtime_CheckedChanged(sender, e);
+        }
+
+        private void cb_show_camera_number_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_show_camera_number, pictureBox_showCameraNo);
+        }
+
+        private void pictureBox_showCameraNo_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_show_camera_number);
+            cb_show_camera_number_CheckedChanged(sender, e);
+        }
+
+        private void cb_show_rec_icon_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_show_rec_icon, pictureBox_showRecordingIcon);
+        }
+
+        private void pictureBox_showRecordingIcon_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_show_rec_icon);
+            cb_show_rec_icon_CheckedChanged(sender, e);
+        }
+
+        private void cb_face_recognition_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_face_recognition, pictureBox_faceRecognition);
+        }
+
+        private void pictureBox_faceRecognition_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_face_recognition);
+            cb_face_recognition_CheckedChanged(sender, e);
+        }
+
+        private void cb_backlight_on_recognition_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_backlight_on_recognition, pictureBox_BacklightOnRecognition);
+        }
+
+        private void pictureBox_BacklightOnRecognition_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_backlight_on_recognition);
+            cb_backlight_on_recognition_CheckedChanged(sender, e);
+        }
+
+        private void cb_operator_capture_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_operator_capture, pictureBox_operatorCapture);
+        }
+
+        private void pictureBox_operatorCapture_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_operator_capture);
+            cb_operator_capture_CheckedChanged(sender, e);
+        }
+
+        private void cb_record_upon_start_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_record_upon_start, pictureBox_recordUponStart);
+        }
+
+        private void pictureBox_recordUponStart_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_record_upon_start);
+            cb_record_upon_start_CheckedChanged(sender, e);
+        }
+
+        private void cb_recording_during_facerec_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_recording_during_facerec, pictureBox_recordingDuringFaceRecognition);
+        }
+
+        private void pictureBox_recordingDuringFaceRecognition_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_recording_during_facerec);
+            cb_recording_during_facerec_CheckedChanged(sender, e);
+        }
+
+        private void cb_backlight_off_idling_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_backlight_off_idling, pictureBox_backlightOffWhenIdle);
+        }
+
+        private void pictureBox_backlightOffWhenIdle_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_backlight_off_idling);
+            cb_backlight_off_idling_CheckedChanged(sender, e);
+        }
+
+        private void cb_event_recorder_CheckedChanged(object sender, EventArgs e)
+        {
+            Img_Chenge(cb_event_recorder, pictureBox_EventRecorder);
+        }
+
+        private void pictureBox_EventRecorder_Click(object sender, EventArgs e)
+        {
+            CB_Chenge(cb_event_recorder);
+            cb_event_recorder_CheckedChanged(sender, e);
+        }
+
+
+        //Function ----> PictureBox Chenge Action
+        private void Img_Chenge(CheckBox ChkObj, PictureBox ImgObj)
+        {
+            if (ChkObj.Checked == true)
+            {
+                ImgObj.Image = Properties.Resources.check;
+            }
+            else
+            {
+                ImgObj.Image = Properties.Resources.checkered;
+            }
+        }
+
+        //Function ----> CheckBox Chenge Action
+        private void CB_Chenge(CheckBox ChBox)
+        {
+            if (ChBox.Checked == true)
+            {
+                ChBox.Checked = false;
+            }
+            else
+            {
+                ChBox.Checked = true;
+            }
+        }
+
+        private void button_cameraProperties_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
