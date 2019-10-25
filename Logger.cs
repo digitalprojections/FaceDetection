d04817fdb4f7f85bbe55722f6fee1cec861eedab
digitalprojects <fuzalov@gmail.com>
2019-10-25 14:10:58 +0900

    logger added

---
+++ Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FaceDetection
+{
+    public class Logger
+    {
+        public Logger()
+        {
+            
+        }
+
+        public static void Add(string logMessage)
+        {
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+                w.WriteLine("  :");
+                w.WriteLine($"  :{logMessage}");
+                w.WriteLine("-------------------------------");
+            }
+            
+        }
+    }
+}
