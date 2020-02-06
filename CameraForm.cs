7cdca67550ede92863ac50aa353b0bd2ccfeee93
BartRob <bartrox92@gmail.com>
2020-02-06 17:05:59 +0900

    minimized sate didn't work when onTop

--- CameraForm.cs
+++ CameraForm.cs
@@ -344,7 +344,7 @@ namespace FaceDetection
             {
                 this.Activate();
             }
-            else if(PROPERTY_FUNCTIONS.CheckOnTopByIndex(CameraIndex)) // Bring sub to the front if "on top" is selected
+            else if(PROPERTY_FUNCTIONS.CheckOnTopByIndex(CameraIndex) && this.WindowState != FormWindowState.Minimized) // Bring sub to the front if "on top" is selected
             {
                 if (PROPERTY_FUNCTIONS.CheckFullScreenByIndex(CameraIndex))
                 {
